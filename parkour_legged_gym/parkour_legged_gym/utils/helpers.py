2e4cc324d53e17010850d2cccea86b2a4d44972f
Tianshu <tian-shu@sjtu.edu.cn>
2024-05-25 14:00:53 +0800

    set default task to lite3

--- parkour_legged_gym/parkour_legged_gym/utils/helpers.py
+++ parkour_legged_gym/parkour_legged_gym/utils/helpers.py
@@ -181,7 +181,7 @@ def update_cfg_from_args(env_cfg, cfg_train, args):
 
 def get_args():
     custom_parameters = [
-        {"name": "--task", "type": str, "default": "a1", "help": "Resume training or start testing from a checkpoint. Overrides config file if provided."},
+        {"name": "--task", "type": str, "default": "lite3", "help": "Resume training or start testing from a checkpoint. Overrides config file if provided."},
         {"name": "--resume", "action": "store_true", "default": False,  "help": "Resume training from a checkpoint"},
         {"name": "--experiment_name", "type": str,  "help": "Name of the experiment to run or load. Overrides config file if provided."},
         {"name": "--run_name", "type": str,  "help": "Name of the run. Overrides config file if provided."},
