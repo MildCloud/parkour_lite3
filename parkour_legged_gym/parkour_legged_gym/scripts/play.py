a8eb6cd359ea69d7eac9b47d24f5e8ee1a636d33
Tianshu <tian-shu@sjtu.edu.cn>
2024-06-01 13:36:02 +0800

    modify play envs and urdf

--- parkour_legged_gym/parkour_legged_gym/scripts/play.py
+++ parkour_legged_gym/parkour_legged_gym/scripts/play.py
@@ -66,7 +66,7 @@ def play(args):
     # override some parameters for testing
     if args.nodelay:
         env_cfg.domain_rand.action_delay_view = 0
-    env_cfg.env.num_envs = 16 if not args.save else 64
+    env_cfg.env.num_envs = 4 if not args.save else 64
     env_cfg.env.episode_length_s = 60
     env_cfg.commands.resampling_time = 60
     env_cfg.terrain.num_rows = 5
